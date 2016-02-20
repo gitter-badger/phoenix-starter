5c20a9ce8f11868409e0163193a065f432741802
Michael Cloutier <viotech@comcast.net>
2016-02-20 09:36:13 -0500

    Update dependencies.

--- mix.exs
+++ mix.exs
@@ -2,20 +2,19 @@ defmodule PhoenixGuardian.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :phoenix_guardian,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases,
-     deps: deps]
+    [
+      app: :phoenix_guardian,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      elixirc_paths: elixirc_paths(Mix.env),
+      compilers: [:phoenix] ++ Mix.compilers,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      aliases: aliases,
+      deps: deps
+    ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {PhoenixGuardian, []},
@@ -45,38 +44,30 @@ defmodule PhoenixGuardian.Mixfile do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
-    [{:ex_machina, "~>0.6", only: [:dev, :test]},
-     {:phoenix, "~> 1.1.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:postgrex, ">= 0.0.0", override: true},
-     {:phoenix_html, "~> 2.3"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:guardian_db, "0.4.0"},
-     {:guardian, "0.9.0"},
-     {:ueberauth, git: "https://github.com/ueberauth/ueberauth.git", override: true},
-     {:ueberauth_github, "~>0.2.0"},
-     {:ueberauth_identity, "~>0.2.1"},
-     {:ueberauth_slack, "~>0.2.0"},
-     {:ueberauth_google, "~> 0.2"},
-     {:ueberauth_facebook, "~> 0.3.2"},
-     {:comeonin, "~> 1.6"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:ex_machina, "~>0.6", only: [:dev, :test]},
+      {:phoenix, "~> 1.1.4"},
+      {:phoenix_ecto, "~> 2.0"},
+      {:postgrex, ">= 0.0.0", override: true},
+      {:phoenix_html, "~> 2.5"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:guardian_db, "0.4.0"},
+      {:guardian, "0.9.0"},
+      {:ueberauth, git: "https://github.com/ueberauth/ueberauth.git", override: true},
+      {:ueberauth_github, "~>0.2.0"},
+      {:ueberauth_identity, "~>0.2.1"},
+      {:ueberauth_slack, "~>0.2.0"},
+      {:ueberauth_google, "~> 0.2"},
+      {:ueberauth_facebook, "~> 0.3.2"},
+      {:comeonin, "~> 1.6"},
+      {:cowboy, "~> 1.0"}
+    ]
   end
 
-  # Aliases are shortcut or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
